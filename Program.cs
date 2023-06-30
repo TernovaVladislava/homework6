ee4fbabc09ca60d22bd0f4f14f131dcdc24accf9
Vlada <vlada-ternova@mail.ru>
2023-06-30 14:15:52 +0300

    Задача 1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿int[] InputArray(int length)
+{
+    int[] array = new int (length);
+    for(int i = 0; i < array.Length; i++)
+    {
+        array [i] = Prompt($"Введите {i+1}-й элемент");
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i< array.Length; i++)
+    {
+        Console.WriteLine($" a[{i}] = {array[i]}");
+    }
+}
+int CountPositiveNumbers(int[] arrau)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0 )
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int length = Prompt("Введите количество элементов >");
+int[] array;
+array = InputArray(length);
+PrintArray (array);
+Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(array)}");
